50428eecfdd07814329570c4522a635705064e0f
kherma <kamil.herma01@gmail.com>
2021-05-09 22:46:44 +0200

    Add mobile queries to navbar

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -3,6 +3,8 @@ import styles from './Header.module.scss';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
+import { FaBars } from 'react-icons/fa';
+
 import CameraLogo from '../../common/CameraLogo/CameraLogo';
 import Navbar from '../Navbar/NavbarContainer';
 import ThemeSwitch from '../../feature/ThemeSwitch/ThemeSwitchContainer';
@@ -16,8 +18,16 @@ const Header = ({ theme }) => {
       )}
     >
       <CameraLogo />
-      <Navbar />
+      {window.matchMedia('(min-width: 768px)').matches && <Navbar />}
       <ThemeSwitch />
+      {window.matchMedia('(max-width: 767px)').matches && (
+        <FaBars
+          className={clsx(
+            styles.icon,
+            theme === 'light' ? styles.light : styles.dark
+          )}
+        />
+      )}
     </header>
   );
 };
--- src/components/layout/Header/Header.test.js
+++ src/components/layout/Header/Header.test.js
@@ -3,6 +3,18 @@ import Header from './Header';
 import { shallow } from 'enzyme';
 
 describe('Header Component', () => {
+  beforeEach(() => {
+    window.matchMedia = (query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    });
+  });
   it('should render without crashing', () => {
     const component = shallow(<Header />);
     expect(component).toBeTruthy();
