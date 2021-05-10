d2dec707c22c72b35c4c88e9cca97114dba91abc
kherma <kamil.herma01@gmail.com>
2021-05-10 22:32:18 +0200

    Add ButtonSave component

---
+++ src/components/common/ButtonSave/ButtonSave.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ButtonSave.module.scss';
+
+const ButtonSave = () => {
+  return <button className={styles.root}>save</button>;
+};
+
+export default ButtonSave;
