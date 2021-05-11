b39cc4bc378f6dc459e433da82f8ca9cb880b853
kherma <kamil.herma01@gmail.com>
2021-05-11 14:08:52 +0200

    Add FavoritesPage layout

--- src/components/view/FavoritesPage/FavoritesPage.js
+++ src/components/view/FavoritesPage/FavoritesPage.js
@@ -2,11 +2,33 @@ import React, { useEffect } from 'react';
 import styles from './FavoritesPage.module.scss';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+
+import ImageItem from '../../common/ImageItem/ImageItemContainer';
+import ImageItemMobile from '../../common/ImageItemMobile/ImageItemMobileContainer';
+import MasonryGrid from '../../layout/MasonryGrid/MasonryGridContainer';
 
 const FavoritesPage = ({ data, loading }) => {
   return (
     <div className={clsx(styles.root)}>
-      <h2>FavoritesPage</h2>
+      {data.length ? (
+        <MasonryGrid>
+          {data.map((item) => {
+            return window.matchMedia('(min-width: 1199px)').matches ? (
+              <ImageItem key={item.id} {...item} />
+            ) : (
+              <ImageItemMobile key={item.id} {...item} />
+            );
+          })}
+        </MasonryGrid>
+      ) : (
+        <div className={styles.container}>
+          <p className={styles.text}>You haven&apos;t saved any Pins yet</p>
+          <Link to="/" className={styles.btnLink}>
+            Find ideas
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
@@ -14,6 +36,10 @@ const FavoritesPage = ({ data, loading }) => {
 FavoritesPage.propTypes = {
   data: PropTypes.array,
   loading: PropTypes.object,
+};
+
+FavoritesPage.defaultProps = {
+  data: [],
 };
 
 export default FavoritesPage;
